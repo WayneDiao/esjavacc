42eb607a3205e9e5d4c64c333613c06da4aee92f
licf <licf@hua-cloud.com.cn>
2017-09-06 19:06:33 +0800

    添加解析bool query

--- src/main/java/com/huacloud/es/impl/BoolQueryImpl.java
+++ src/main/java/com/huacloud/es/impl/BoolQueryImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.huacloud.es.BoolQuery;
 import com.huacloud.es.Col;
 import com.huacloud.es.Exp;
+import com.huacloud.es.Opt;
 
 import java.util.Arrays;
 
@@ -23,35 +24,77 @@ public class BoolQueryImpl implements BoolQuery{
     }
 
     private JSONObject getBoolQuery(Exp exp){
-        JSONObject bool=new JSONObject();
-        JSONObject must=new JSONObject();
-
-        bool.put("bool","");
-        return bool;
-    }
-
-    private JSONObject getMust(Col col){
+        if(exp instanceof Col){
+            Col col=(Col)exp;
+            if(Opt.NOT_EQUAL.equals(col.getOpt())){
+                return getMustNot(getTerm(col));
+            }else{
+                return getMust(getTerm(col));
+            }
+        }
         return null;
     }
 
-    private JSONObject getMustNot(Col col){
-        return null;
+    private JSONObject getBool(JSONObject jsonObject){
+       return createKeyQuery("bool",jsonObject);
+    }
+
+    private JSONObject getMust(JSONObject jsonObject){
+        return createKeyQuery("must",jsonObject);
+    }
+
+    private JSONObject getMustNot(JSONObject jsonObject){
+        return createKeyQuery("must_not",jsonObject);
     }
 
 
-    private JSONObject getFilter(Col col){
-        return null;
+    private JSONObject getFilter(JSONObject jsonObject){
+       return createKeyQuery("filter",jsonObject);
+    }
+
+    private JSONObject createKeyQuery(String key,JSONObject jsonObject){
+        JSONObject keyObject=new JSONObject();
+        keyObject.put(key,jsonObject);
+        return keyObject;
     }
 
     private JSONObject getTerm(Col col){
-       return null;
+        JSONObject term=new JSONObject();
+        JSONObject property=new JSONObject();
+        property.put(col.getProperty(),col.getValue());
+        term.put("term",property);
+       return term;
     }
 
     private JSONObject getRange(Col left,Col right){
         JSONObject range=new JSONObject();
-        range.put(left.getProperty(),left.getValue());
+        JSONObject property=new JSONObject();
+        String opLeft=null,opRight=null;
+        if(Opt.GT.equals(left.getOpt())){
+            opLeft="gt";
+        }else if(Opt.GTE.equals(left.getOpt())){
+            opLeft="gte";
+        }else if(Opt.lT.equals(left.getOpt())){
+            opLeft="lt";
+        }else if(Opt.lTE.equals(left.getOpt())){
+            opLeft="lte";
+        }
+        if(Opt.GT.equals(right.getOpt())){
+            opRight="gt";
+        }else if(Opt.GTE.equals(right.getOpt())){
+            opRight="gte";
+        }else if(Opt.lT.equals(right.getOpt())){
+            opRight="lt";
+        }else if(Opt.lTE.equals(right.getOpt())){
+            opRight="lte";
+        }
+
+        property.put(opLeft,left.getValue());
+        property.put(opRight,right.getValue());
+        range.put(left.getProperty(),property);
         return range;
     }
+
 
 
 
